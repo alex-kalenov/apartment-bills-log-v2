3bc3076f024967b6c841259753dca3a0f763b5c1
Alexey <54000752+alex-kalenov@users.noreply.github.com>
2021-12-22 11:30:21 +0200

    A little fix

--- src/components/UI/Toast.js
+++ src/components/UI/Toast.js
@@ -1,15 +1,11 @@
 import styles from "./Toast.module.css";
 
-import { useContext } from "react";
 import { useSelector } from "react-redux";
-
-import MsgContext from "../../store/message-context";
 
 const Toast = (props) => {
   const visible = useSelector((state) => state.msg.visible);
   const type = useSelector((state) => state.msg.type);
   const message = useSelector((state) => state.msg.message);
-  //const msgCtx = useContext(MsgContext);
 
   const toastClass = `${styles.toast} ${
     type === "succeed" ? styles.succeed : styles.error
